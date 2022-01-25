66f89d2f8ec146230bf5ee55e83889f4fa5111aa
EasyUM-8 <natalia.devpro@icloud.com>
2022-01-25 20:25:21 +0300

    исправлен баг при регистрации

--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -3,7 +3,8 @@ package lesson6;
 public class Main {
     public static void main(String[] args) {
 
-        StringUtils.reverseCharInWords("qwerty hello world!!!");
+        StringUtils.reverseCharInWords("jbhj hello world!!!");
+        System.out.println("kdhfdrf");
 
     }
 }
